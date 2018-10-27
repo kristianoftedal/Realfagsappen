2bbb49464b07cfccf29b10161434d0b2d396f235
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-10-27 15:42:32 +0200

    new questions

--- src/questions/s1/categories.js
+++ src/questions/s1/categories.js
@@ -4,6 +4,7 @@ const categories = [
   { value: 'Likninger og ulikheter', isSelected: false },
   { value: 'Funksjoner og regresjon', isSelected: false },
   { value: 'Derivasjon', isSelected: false },
+  { value: 'Sannsynlighet', isSelected: false },
   { value: 'Funksjonsdrøfting', isSelected: false },
   { value: 'Lineær optimering', isSelected: false },
 ];
