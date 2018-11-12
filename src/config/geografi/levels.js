16b9398951683a5e4afcf7460f87478c77d470f9
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-11-12 21:11:41 +0100

    geo levels

--- src/config/geografi/levels.js
+++ src/config/geografi/levels.js
@@ -13,6 +13,7 @@ import twelve from '../../images/levels/geografi/12.png';
 import thirteen from '../../images/levels/geografi/13.png';
 import fourteen from '../../images/levels/geografi/14.png';
 import fifteen from '../../images/levels/geografi/15.png';
+import blackhole from '../../images/levels/kjemi1/sorthull.png';
 
 const levels = [
   {
@@ -21,100 +22,100 @@ const levels = [
     text: 'Nybegynner',
   },
   { 
-    value: '1',
+    value: 'Kornproduksjon',
     score: 500,
     imageSource: one,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Korn dyrkes for å kunne benyttes som føde for mennesker og dyr. I 2018 dyrket Norge ca. 200.000 tonn matkorn. Åkerlandskapet er synlig, særlig på flatbygdene i Norge og er en del av primærnæringen. Færre mennesker jobber i landbruket, men mekaniseringen har gjort at selvforsyningsgraden i Norge ligger på 50%.'
   },
   { 
-    value: '2',
+    value: 'Husdyrhold',
     score: 1300,
     imageSource:  two,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Husdyr er dyr som har blitt domestisert for å tilfredsstille menneskelige behov, primært gjennom avl. Typiske eksempler er gårdsdyr som ku, sau, geit, gris, osv. Disse brukes hovedsakelig til henholdsvis kjøtt- og melkeproduksjon. Slik type produksjon finner vi i fjell- og dalstrøk, og i andre områder der det er ugunstig å drive med kornproduksjon.'
   },
   { 
-    value: '3',
+    value: 'Fiskerinæring',
     score: 2300,
     imageSource:  three,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Norsk fiskerihistorie går tilbake til steinalderen. De første innvandrerne til Norge oppdaget tidlig at det utenfor kysten, fantes rike forekomster av fisk, sel, og hval. På 1100-tallet startet eksporten av tørket torsk og sild til England. Norge fikk et eget fiskeridepartement i 1946, og i dag eksporterer fiskerinæringen fisk til hele verden. Mange steder i verden anses norsk laks som den fineste råvare. Fiske er fortsatt en av Norges største eksportnæringer, og er en del av primærnæringen.'
   },
   {
-    value: '4',
+    value: 'Hogst- og tømmerindustri',
     score: 4500,
     imageSource:  four,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Skogen har fra de eldste tider vært en viktig del av næringsgrunnlaget i Norge. Den gav mat, brensel, redskaper og hus. Allerede på 1300-tallet startet eksporten av trelast fra Norge til andre land i Europa. I dag antas det at ca. 30.000 mennesker er involvert i hogst- og tømmerindustrien i Norge, og dette er en del av primærnæringen.'
   },
   {
-    value: '5',
+    value: 'Kobbergruver',
     score: 7000,
     imageSource:  five,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Kobber har blitt brukt i de eldste av sivilisasjoner, og kan spores over 10.000 år tilbake. Kobber er et viktig krigsmetall, brukt til framstilling av for eksempel kanoner. C14-dateringer fra Meråker i Nord-Trøndelag viser at kobbergruver har eksistert i Norge siden slutten av 1100-tallet. Et av de mest kjente kobberverkene i Norge ligger på Røros, og regnes som en del av det som kalles for Trondheimfeltet.'
   },
   {
-    value: '6',
+    value: 'Sølvgruver',
     score: 10000,
     imageSource:  six,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'De første forsøkene på å utvinne sølv fra gruver er 6.000 år gamle. Bevisene tyder på at dette startet i Anatolia ved Middelhavet. I Norge derimot, startet sølvjakten rundt år 1600. Kongen av Norge og Danmark, Christian IV, etablerte byen Kongsberg i Buskerud etter oppdagelsen av sølv i 1623.  I dag er 300 gruvesjakter fortsatt intakt.'
   },
   {
-    value:'7',
+    value:'Kullgruver',
     score: 15000,
     imageSource:  seven,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'En frodig landvegetasjon er et krav for dannelsen av kull. De største ansamlingene av kull har blitt dannet når jorden har hatt et varmt og fuktig klima, hovedsakelig i de geologiske periodene karbon og kritt. I Norge er det svært begrensede mengder kull. De første kullfunnene ble gjort på 1800-tallet, men kommersielt utvinnbare reserver finnes kun på Svalbard.'
   },
   {
-    value:'8',
+    value: 'Papir- og pappindustri',
     score: 20000,
     imageSource:  eight,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'På slutten av 1800-tallet ble det etablert en rekke tresliperier, celluloseanlegg og papirfabrikker i landet. I Norge har det meste av papirindustrien vært lokalisert på Østlandet og i Trøndelag. Områder som også har de mest imponerende skogsarealene. Mye av produksjonen har skjedd ved elvenes utløp, særlig der Drammensvassdraget og Glomma renner ut. Dette fordi man ville fløte tømmer direkte til fabrikken og eksportere det ferdige produktet ut i verden. Norske Skog er verdens nest største produsent av avispapir.'
   },
   {
-    value: '9',
+    value: 'Turistnæring',
     score: 25000,
     imageSource:  nine,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'I 1868 ble Den Norske Turistforening stiftet for å arbeide med fotturismens utbredelse i Norge. Dette var starten på en storsatsning innen norsk turisme. Etterhvert kom det cruise-skip som reiste langs Vestlandet, samtidig som turister på bil- og campingferier i Norge vokste. Etter 1945 har det vært en eksplosiv vekst i reiselivet, dette gjelder både turisme innlands og utenlands.'
   },
   {
-    value: '10',
+    value: 'Aluminiumsverk',
     score: 30000,
     imageSource:  ten,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Norge er blant verdens største aluminiumprodusenter. Mye av aluminiumen som utvinnes i Norge kommer fra gruveselskaper i Brasil som utvinner aluminiumoksid. På grunn av den rikelig tilgangen på elektrisk energi, som i Norge hovedsakelig kommer fra vannkraft, fremstilles aluminiumen ved aluminiumsverkene på Vestlandet. Norsk Hydro, Alcoa og Alcan har til sammen sju smelteverk fra Lista i sør, til Mosjøen i nord. Produksjon av aluminium startet i Norge i 1908. Ca. 3.000 mennesker jobber i aluminiumsnæringen.'
   },
   { 
-    value: '11',
+    value: 'Vannkraft',
     score: 37000,
     imageSource:  eleven,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Vannkraft har vært hovedkilden til produksjonen av energi og strøm i Norge de siste 100 årene. På grunn av fjellandskapet og den store nedbørsmengder i Norge, har utbygging av vannkraft vært spesielt gunstig her til lands. Dette var en forutsetting for industrialiseringen av Norge på begynnelsen av 1900-tallet. I Norge kommer mellom 95% og 99% av all elektrisk energiproduksjon fra den fornybare energikilden vannkraft.'
   },
   { 
-    value: '12',
+    value: 'Kunstgjødsel',
     score: 45000,
     imageSource:  twelve,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Norsk Hydro har siden 1930-årene vært en av verdens største produsenter av kunstgjødsel. De norske vitenskapsmennene Kristian Birkeland og Sam Eyde utviklet en metode for å høste nitrogen fra luft, og denne metoden dannet grunnlaget for selskapet. Ammoniakk dannes fra nitrogen og brukes til å produsere næringssalter som plantene trenger for å vokse. Dette har revolusjonert matproduksjonen, og dermed bidratt til befolkningsvekst.'
   },
   { 
-    value: '13',
+    value: 'Gassutvinning',
     score: 60000,
     imageSource:  thirteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'I 1959 ble det gjort et stort naturgassfunn i Nederland. Dette funnet gjorde at store foretak kontaktet Norge med ønske om å lete etter gass utenfor norskekysten. Gass ble funnet, og det viste seg å være snakk om enorme gassreserver. De fleste felt som produserer gass, produserer også olje. I dag er det 56 gassfelt i produksjon, eller under utbygging i Norge. Åtte av disse drives kun som gassfelt.'
   },
   { 
-    value: '14',
+    value: 'Oljeproduksjon',
     score: 75000,
     imageSource:  fourteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Norsk oljehistorie handler om funn og utvinning av petroleum på norsk sokkel fra 1960-årene og frem til i dag. Oljevirksomheten ble en dominerende faktor i norsk økonomi, og er i dag Norges største næring målt i verdiskaping, statlige inntekter, investeringer og eksportverdi. Totalt er det om lag 50 store og små oljefelt i produksjon, eller under utbygging.'
   },
   { 
-    value: '15',
+    value: 'Teknologi og forskning',
     score: 90000,
     imageSource:  fifteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Norge bruker rundt 50 milliarder kroner på forskning og utvikling i året. Det tilsvarer 1,65% av bruttonasjonalproduktet (BNP). Andelen har vært den samme i mange år. Om lag 70.000 ansatte jobber med forskning og utvikling i Norge. Norge bli mer og mer avhengig av forskning og inntektene fra dette, når det fossile brenslet tar slutt. Gratulerer med å ha oppnådd det høyeste nivået i Geografi-appen!'
   },
   { 
-    value: 'Universet',
+    value: 'Sort hull',
     score: 30000000000000,
-    imageSource:  fifteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    imageSource:  blackhole,
+    text: 'Et sort hull eller svart hull er den fysiske effekten av et objekt som har en masse tilsvarende 3-4 ganger solens og oppover, og som har ekstremt høy tetthet. Yttergrensen til et sort hull kalles hendelseshorisont, og innenfor denne er gravitasjonskreftene så store at ingenting kan unnslippe - ikke engang lys.'
   },
 ];
 
