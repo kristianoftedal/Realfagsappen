7a2976b11e36a7c058fb50818cc23cc46f6c9591
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-11-14 13:48:43 +0100

    1t levels

--- src/config/1t/levels.js
+++ src/config/1t/levels.js
@@ -22,94 +22,94 @@ const levels = [
     text: 'Nybegynner',
   },
   { 
-    value: '1',
+    value: 'Kule',
     score: 500,
     imageSource: one,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'En kule er en perfekt symmetrisk figur som ofte omtales som en sfære. Kjente eksempler på kuler er klinkekuler, baller og planeter. Det er derimot svært vanskelig å lage en perfekt kule, så vi finner de ikke i naturen.'
   },
   { 
-    value: '2',
+    value: 'Halvkule',
     score: 1300,
     imageSource:  two,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'En halvkule, også kalt hemisfære, oppstår når en kule deles i to like store deler. Det mest brukte eksemplet på halvkuler er inndelingen av jorda i den nordlige og sørlige halvkulen.'
   },
   { 
-    value: '3',
+    value: 'Ellipsiode',
     score: 2300,
     imageSource:  three,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'En ellipsoide er en tredimensjonal geometrisk figur som kan sees på som den tredimensjonale utgaven av en ellipse. Det er mer korrekt å kalle jorda og andre planeter for ellipsioder, siden de er litt større ved ekvator enn det en kule ville vært.'
   },
   {
-    value: '4',
+    value: 'Kjegle',
     score: 4500,
     imageSource:  four,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Kjegler finner vi mange varianter av i naturen, selv om ingen objekter i naturen er perfekte kjegler. Grantrær, fjell og skjell danner ofte former som ligner på kjegler. Den mest vanlige kjeglen å observere om sommeren, er kjeksen på en kroneis.'
   },
   {
-    value: '5',
+    value: 'Kube',
     score: 7000,
     imageSource:  five,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Kuber forbindes ofte med terninger, men du trenger ikke se deg rundt lenge før du finner andre eksempler på kuber. Både isbiter og sukkerbiter er ofte formet som kuber. En kube har 6 like store sider, og alle disse er perfekte kvadrater.'
   },
   {
-    value: '6',
+    value: 'Parallellepiped',
     score: 10000,
     imageSource:  six,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Parallellepipeder finner vi ikke i naturen, men de har blitt brukt som inspirasjon av mennesker for å lage ulike gjenstander i lang tid. For elever er det nok viskelæret som minner om denne strukturen. I moderne tid har denne formen blitt brukt til å lage hus, men da av estetiske årsaker fremfor arkitektoniske.'
   },
   {
-    value:'7',
+    value: 'Sylinder',
     score: 15000,
     imageSource:  seven,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Sylinder er en form som dukker ofte opp i naturen. Trestammer og stilker er nok de mest kjente eksemplene. Sylinderformen dukker ofte opp i gjenstander skapt av mennesker. Brusbokser, glass, søppelbøtter og Pringles-bokser har ofte sylinderform.'
   },
   {
-    value:'8',
+    value: 'Pyramide',
     score: 20000,
     imageSource:  eight,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Pyramider er lette å kjenne igjen, da de ble brukt av mennesker i det gamle Eqypt som gravkamre for faraoene. Vi har funnet over 130 pyramider i Eqypt og minst 300 til rundt omkring i verden. Årsaken til at pyramidestrukturen ble valgt, er mest sannsynlig for at den døde faraoen lettere skulle kunne stige opp mot himmelen.'
   },
   {
-    value: '9',
+    value: 'Rektangulært prisme',
     score: 25000,
     imageSource:  nine,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Rektangulære prismer er figurer vi kan kjenne igjen fra hverdagen, som for eksempel et akvarie eller esken til en frokostblanding. Andre former for rektangulære prismer er høyblokker eller containere.'
   },
   {
-    value: '10',
+    value: 'Trekantet prisme',
     score: 30000,
     imageSource:  ten,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Prismer brukes svært ofte i optikk. Et kjent eksperiment er å sende lys gjennom et prisme slik at prismet bryter lyset, og gir oss alle de synlige fargene i regnbuen. I hverdagen ser vi et trekantet prisme hver gang vi er i godteriavdelingen og plukker opp en Toblerone.'
   },
   { 
-    value: '11',
+    value: 'Tetraeder',
     score: 37000,
     imageSource:  eleven,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Tetraeder forekommer i naturen, men oftest på steder der vi ikke ser dem. I molekyler med elektronparbindinger, som for eksempel i metan, er strukturen et perfekt tetraeder. Dette er for å få mest mulig avstand mellom alle hydrogenatomene.'
   },
   { 
-    value: '12',
+    value: 'Oktaeder',
     score: 45000,
     imageSource:  twelve,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Oktaeder er en form som svært verdifulle diamanter, krystaller og juveler har. De kan se ut som to pyramider satt sammen. Et kjent eksempel er en variant av Rubiks kube formet som et oktaeder, som er mye mer komplisert enn en standard Rubiks kube.'
   },
   { 
-    value: '13',
+    value: 'Dodekaeder',
     score: 60000,
     imageSource:  thirteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Dodekaeder finner vi i naturen i enkelte frukter. Det mest kjente eksempelet er granateplefrø og kvartsstener. Et tilfelle man muligens opplever hver dag er strukturen vi ser hos såpebobler. Disse har faktisk en klassisk dodekaeder struktur.'
   },
   { 
-    value: '14',
+    value: 'Ikosaeder',
     score: 75000,
     imageSource:  fourteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Vi finner både naturlige og syntetiske ikosaeder. Ikosaeder-formede spillterninger er vanlig i mange rollespill, blant annet det kjente spillet Dungeons & Dragons. Mange virus, som for eksempel herpesviruset, har skall som er formet som et ikosaeder.'
   },
   { 
-    value: '15',
+    value: 'Torus',
     score: 90000,
     imageSource:  fifteen,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    text: 'Mange forskere antar at universet er formet som en torus. Gratulerer med å ha oppnådd det høyeste nivået i 1T-appen, dette bør feires med et torus-formet bakverk – en donut!'
   },
   { 
     value: 'Sort hull',
