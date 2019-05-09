954c6e1386bb1de9530be3963cbcce30f14d700f
kristianoftedal <kristian.oftedal@novanet.no>
2019-05-09 21:55:59 +0200

    restore

---
+++ src/config/subjectCodeProvider.js
@@ -0,0 +1,47 @@
+import { NATURFAG, KJEMI1, KJEMI2, S1, ONET, GEO, FYSIKK1, R1, ONEP, NATURFAG_P, NATURFAG_Y, BIO1, BIO2} from '../stores/constants';
+
+
+const subjectCode = (topic) => {
+  if (topic === NATURFAG) {
+    return 'naturfag';
+  }
+  if (topic === KJEMI1) {
+    return 'kjemi1';
+  }
+  if (topic === KJEMI2) {
+    return 'kjemi2';
+  }
+  if (topic === FYSIKK1) {
+    return 'fysikk1';
+  }
+  if (topic === ONET) {
+    return '1t';
+  }
+  if (topic === S1) {
+    return 's1';
+  }
+  if (topic === GEO) {
+    return 'geografi';
+  }
+  if (topic === ONEP) {
+    return '1p';
+  }
+  if (topic === R1) {
+    return 'r1';
+  }
+  if (topic === NATURFAG_P) {
+    return 'naturfag';
+  }
+  if (topic === NATURFAG_Y) {
+    return 'naturfag';
+  }
+  if (topic === BIO1) {
+    return 'bio1';
+  }
+  if (topic === BIO2) {
+    return 'bio2';
+  }
+  return 'naturfag';
+};
+
+export default subjectCode;
